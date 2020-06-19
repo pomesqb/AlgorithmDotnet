7a91e064fbde12a0b348dfdc5dbe593035fbc480
pomesqb <pomesqb@gmail.com>
2020-06-19 16:24:01 +0800

    reverse word completed

--- AlgorithmDotnet/ReverseStringInplace/AlgorithmReverseStringInplace.cs
+++ AlgorithmDotnet/ReverseStringInplace/AlgorithmReverseStringInplace.cs
@@ -6,25 +6,117 @@ namespace AlgorithmDotnet.ReverseStringInplace
 {
     public class AlgorithmReverseStringInplace
     {
-        public string ReverseStringInplace(string inputStr)
+        public char[] ReverseStringInplace(char[] message)
         {
-            var inputSB = new StringBuilder(inputStr);
+            return ReverseCharacter(message, 0, message.Length - 1);
+        }
+
+        public char[] ReverseWords(char[] message)
+        {
+            int leftWordIndex = 0;
+            int rightWordIndex = 0;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if (message[i] == ' ')
+                    rightWordIndex++;
+            }
+
+            int leftWordHeadIndex = 0;
+            int leftWordTailIndex = 0;
+            int rightWordHeadIndex = 0;
+            int rightWordTailIndex = message.Length - 1;
+
+            while (leftWordIndex < rightWordIndex)
+            {
+                for (int i = leftWordHeadIndex; i < message.Length; i++)
+                {
+                    if (message[i] == ' ')
+                    {
+                        leftWordTailIndex = i - 1;
+                        break;
+                    }
+                }
+
+                for (int i = rightWordTailIndex; i >= 0; i--)
+                {
+                    if (message[i] == ' ')
+                    {
+                        rightWordHeadIndex = i + 1;
+                        break;
+                    }
+                }
+
+                int leftWordLength = (leftWordTailIndex - leftWordHeadIndex) + 1;
+                int rightWordLength = (rightWordTailIndex - rightWordHeadIndex) + 1;
+
+                char[] tempChars = new char[leftWordLength];
+                Array.Copy(message, leftWordHeadIndex, tempChars, 0, leftWordLength);
+
+                for (int i = rightWordHeadIndex; i < (rightWordHeadIndex + rightWordLength); i++)
+                {
+                    if (message[leftWordHeadIndex] == ' ')
+                    {
+                        for (int s = rightWordHeadIndex ; s > leftWordHeadIndex; s--)
+                        {
+                            message[s] = message[s - 1];
+                        }
+                    }
+
+                    message[leftWordHeadIndex] = message[i];
+                    leftWordHeadIndex++;
+                }
+                leftWordHeadIndex++;
+
+                int tempCharIndex = tempChars.Length -1;
+                for (int i = tempCharIndex; i >= 0; i--)
+                {
+                    message[rightWordTailIndex] = tempChars[i];
+                    rightWordTailIndex--;
+                }
+                rightWordTailIndex--;
+
+                leftWordIndex++;
+                rightWordIndex--;
+            }
+
+            return message;
+        }
+
+        public char[] ReverseWordsBest(char[] message)
+        {
+            // Reverse character first
+            ReverseStringInplace(message);
 
             int leftIndex = 0;
-            int rightIndex = inputStr.Length - 1;
 
+            // Reverse every word
+            for (int i = 0; i <= message.Length; i++)
+            {
+                if( i == message.Length || message[i] == ' ')
+                {
+                    ReverseCharacter(message, leftIndex, i - 1);
+                    leftIndex = i + 1;
+                }
+            }
+
+            return message;
+        }
+
+        private char[] ReverseCharacter(char[] message, int leftIndex, int rightIndex)
+        {
             while (leftIndex < rightIndex)
             {
                 char tempChar;
-                tempChar = inputStr[leftIndex];
-                inputSB[leftIndex] = inputSB[rightIndex];
-                inputSB[rightIndex] = tempChar;
+                tempChar = message[leftIndex];
+                message[leftIndex] = message[rightIndex];
+                message[rightIndex] = tempChar;
 
                 leftIndex++;
                 rightIndex--;
             }
 
-            return inputSB.ToString();
+            return message;
         }
     }
 }
--- AlgorithmDotnetTest/ReverseStringInplaceTest.cs
+++ AlgorithmDotnetTest/ReverseStringInplaceTest.cs
@@ -16,14 +16,43 @@ namespace AlgorithmDotnetTest
             _algorithmReverseStringInplace = new AlgorithmReverseStringInplace();
         }
 
-        [TestCase("abcde", "edcba")]
-        [TestCase("abcd", "dcba")]
-        [TestCase("abc123", "321cba")]
-        [TestCase("", "")]
-        public void ReverseStringInplace_WhenCalled_ReturnExpectedValue(string originalStr, string expectedValue)
+        [TestCase(
+            new char[] { 'a', 'b', 'c', 'd', 'e'},
+            new char[] { 'e', 'd', 'c', 'b', 'a'})]
+        [TestCase(
+            new char[] { 'a', 'b', 'c', 'd'},
+            new char[] { 'd', 'c', 'b', 'a'})]
+        [TestCase(
+            new char[] { 'a', 'b', 'c', '1', '2', '3' },
+            new char[] { '3', '2', '1', 'c', 'b', 'a' })]
+        public void ReverseStringInplace_WhenCalled_ReturnExpectedValue(char[] originalStr, char[] expectedValue)
         {
             var result = _algorithmReverseStringInplace.ReverseStringInplace(originalStr);
             Assert.That(result, Is.EqualTo(expectedValue));
+        }
+
+        [TestCase(
+            new char[] { 'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l'},
+            new char[] { 's', 't', 'e', 'a', 'l', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 'c', 'a', 'k', 'e'})]
+        [TestCase(
+            new char[] { 'a', 'b', ' ', 'c', 'd', ' ', 'e', 'f', ' ', 'g', 'h'},
+            new char[] { 'g', 'h', ' ', 'e', 'f', ' ', 'c', 'd', ' ', 'a', 'b' })]
+        [TestCase(
+            new char[] { 'a', ' ', 'c', 'd', ' ', 'f', ' ', 'g', 'h' },
+            new char[] { 'g', 'h', ' ', 'f', ' ', 'c', 'd', ' ', 'a' })]
+        public void ReverseWords_WhenCalled_ReturnExpectedValue(char[] originalStr, char[] expectedValue)
+        {
+            var result = _algorithmReverseStringInplace.ReverseWords(originalStr);
+            Assert.That(result, Is.EquivalentTo(expectedValue));
+        }
+
+        [TestCase(
+            new char[] { 'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l' },
+            new char[] { 's', 't', 'e', 'a', 'l', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 'c', 'a', 'k', 'e' })]
+        public void ReverseWordsBest_WhenCalled_ReturnExpectedValue(char[] originalStr, char[] expectedValue)
+        {
+            var result = _algorithmReverseStringInplace.ReverseWordsBest(originalStr);
+            CollectionAssert.AreEqual(expectedValue, result);
         }
     }
 }
