ff674a072af2cc9b25b2935fa7bfb0aadcaef240
阿斯特雷亞 <pomesqb@gmail.com>
2020-07-27 17:16:00 +0800

    完成InflightEntertainment

---
+++ AlgorithmDotnet/InflightEntertainment/AlgorithmCanTwoMoviesFillFlight.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDotnet.InflightEntertainment
+{
+    public class AlgorithmCanTwoMoviesFillFlight
+    {
+        public bool CanTwoMoviesFillFlight(int[] movieLengths, int flightLength)
+        {
+            var moviesSet = new HashSet<int>();
+
+            foreach (var item in movieLengths)
+            {
+                var targetLength = flightLength - item;
+
+                if (moviesSet.Contains(targetLength))
+                {
+                    return true;
+                }
+
+                moviesSet.Add(item);
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AlgorithmDotnetTest/CanTwoMoviesFillFlight.cs
@@ -0,0 +1,26 @@
+﻿using AlgorithmDotnet.InflightEntertainment;
+using NUnit.Framework;
+
+namespace AlgorithmDotnetTest
+{
+    class CanTwoMoviesFillFlight
+    {
+        AlgorithmCanTwoMoviesFillFlight _algorithmCanTwoMoviesFillFlight;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithmCanTwoMoviesFillFlight = new AlgorithmCanTwoMoviesFillFlight();
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4, 5}, 8, true)]
+        [TestCase(new int[] { 8, 4, 6, 3, 7}, 10, true)]
+        [TestCase(new int[] { 8, 4, 6, 3, 7 }, 19, false)]
+        [TestCase(new int[] { 8, 4, 6, 3, 7 }, 20, false)]
+        public void CanTwoMoviesFillFlight_WhenCalled_ReturnExpectedValue(int[] movieLengths, int flightLength, bool expectedValue)
+        {
+            var result = _algorithmCanTwoMoviesFillFlight.CanTwoMoviesFillFlight(movieLengths, flightLength);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+    }
+}
