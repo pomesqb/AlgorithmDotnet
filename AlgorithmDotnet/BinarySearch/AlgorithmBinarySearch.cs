e493eef537e0c52e35ab2c5d875998cf4c339f47
阿斯特雷亞 <pomesqb@gmail.com>
2020-07-28 11:20:54 +0800

    BinarySearch Completed

---
+++ AlgorithmDotnet/BinarySearch/AlgorithmBinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDotnet.BinarySearch
+{
+    public class AlgorithmBinarySearch
+    {
+        public bool BinarySearch(int[] nums, int target)
+        {
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+
+            while(leftIndex <= rightIndex)
+            {
+                int midIndex = (leftIndex + rightIndex) / 2;
+
+                if (target == nums[midIndex])
+                    return true;
+
+                if(target < nums[midIndex])
+                {
+                    rightIndex = midIndex - 1;
+                }
+                else
+                {
+                    leftIndex = midIndex + 1;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ AlgorithmDotnetTest/BinarySearch.cs
@@ -0,0 +1,34 @@
+﻿using AlgorithmDotnet.BinarySearch;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDotnetTest
+{
+    class BinarySearch
+    {
+        private AlgorithmBinarySearch _algorithmBinarySearch;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithmBinarySearch = new AlgorithmBinarySearch();
+        }
+
+        [TestCase(new int[] { 1, 2, 3, 4 }, 4, true)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 3, true)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 1, true)]
+        [TestCase(new int[] { 1, 2}, 2, true)]
+        [TestCase(new int[] { 1 }, 1, true)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 }, 7, false)]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6 }, 0, false)]
+        [TestCase(new int[] { 1, 2}, -1, false)]
+        [TestCase(new int[] { 2 }, 4, false)]
+        public void BinarySearch_WhenCalled_ReturnExpectedValue(int[] nums, int target, bool expectedValue)
+        {
+            var result = _algorithmBinarySearch.BinarySearch(nums, target);
+            Assert.That(result, Is.EqualTo(expectedValue));
+        }
+    }
+}
