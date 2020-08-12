e61bbafe6da1aa61acf161664b5245e6051d61de
阿斯特雷亞 <pomesqb@gmail.com>
2020-08-12 16:45:16 +0800

    Complete ReverseLinkedList

---
+++ AlgorithmDotnet/LinkedList/AlgorithmLinkedList.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDotnet.LinkedList
+{
+    public class LinkedListedNodeEx
+    {
+        public int Value { get; set; }
+        public LinkedListedNodeEx Next { get; set; }
+
+        public LinkedListedNodeEx(int value)
+        {
+            Value = value;
+        }
+    }
+
+    public class AlgorithmLinkedList
+    {
+        public LinkedListedNodeEx ReverseLinkedList(LinkedListedNodeEx head)
+        {
+            LinkedListedNodeEx currentNode = head;
+            LinkedListedNodeEx nextNode = null;
+            LinkedListedNodeEx previousNode = null;
+
+            while (currentNode != null)
+            {
+                nextNode = currentNode.Next;
+                currentNode.Next = previousNode;
+                previousNode = currentNode;
+                currentNode = nextNode;
+            }
+
+            return previousNode;
+        }
+
+        public string PrintLinkedList(LinkedListedNodeEx node)
+        {
+            string result = "";
+
+            while(node != null)
+            {
+                result += node.Value + ",";
+                node = node.Next;
+            }
+
+            result = result.TrimEnd(',');
+
+            return result;
+        }
+    }
+}
---
+++ AlgorithmDotnetTest/LinkedListTest.cs
@@ -0,0 +1,32 @@
+﻿using AlgorithmDotnet.LinkedList;
+using NUnit.Framework;
+
+namespace AlgorithmDotnetTest
+{
+    class LinkedList
+    {
+        private AlgorithmLinkedList _algorithmLinkedList;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithmLinkedList = new AlgorithmLinkedList();
+        }
+
+        [TestCase]
+        public void ReverseLinkedList_WhenCalled_ReturnExpectedValue()
+        {
+            var node1 = new LinkedListedNodeEx(1);
+            var node2 = new LinkedListedNodeEx(2);
+            var node3 = new LinkedListedNodeEx(3);
+
+            node1.Next = node2;
+            node2.Next = node3;
+
+            var newHead = _algorithmLinkedList.ReverseLinkedList(node1);
+            var result = _algorithmLinkedList.PrintLinkedList(newHead);
+
+            Assert.That(result, Is.EqualTo("3,2,1"));
+        }
+    }
+}
