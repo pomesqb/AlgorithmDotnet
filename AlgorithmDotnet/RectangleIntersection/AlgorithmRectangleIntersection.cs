90f50f06f61ba21ce57eadb4e1ae17921ae47095
阿斯特雷亞 <pomesqb@gmail.com>
2020-08-24 17:31:51 +0800

    RectangleIntersection

---
+++ AlgorithmDotnet/RectangleIntersection/AlgorithmRectangleIntersection.cs
@@ -0,0 +1,80 @@
+﻿namespace AlgorithmDotnet.RectangleIntersection
+{
+    public class Rectangle
+    {
+        public int LeftX { get; set; }
+        public int RightX { get; set; }
+        public int BottomY { get; set; }
+        public int TopY { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+
+        public Rectangle() { }
+
+        public Rectangle(int leftX, int bottomY, int width, int height)
+        {
+            LeftX = leftX;
+            BottomY = bottomY;
+            Width = width;
+            Height = height;
+            RightX = leftX + width;
+            TopY = BottomY + height;
+        }
+    }
+
+    public class AlgorithmRectangleIntersection
+    {
+        public Rectangle RectangleIntersection(Rectangle recFirst, Rectangle recSecond)
+        {
+            var resultRectangle = new Rectangle();
+
+            recFirst.RightX = recFirst.LeftX + recFirst.Width;
+            recFirst.TopY = recFirst.BottomY + recFirst.Height;
+            recSecond.RightX = recSecond.LeftX + recSecond.Width;
+            recSecond.TopY = recSecond.BottomY + recSecond.Height;
+
+            if(recFirst.LeftX <= recSecond.LeftX)
+            {
+                resultRectangle.LeftX = recSecond.LeftX;
+            }
+            else
+            {
+                resultRectangle.LeftX = recFirst.LeftX;
+            }
+
+            if (recFirst.RightX <= recSecond.RightX)
+            {
+                resultRectangle.RightX = recFirst.RightX;
+            }
+            else
+            {
+                resultRectangle.RightX = recSecond.RightX;
+            }
+
+            resultRectangle.Width = resultRectangle.RightX - resultRectangle.LeftX;
+
+            // Y
+            if (recFirst.BottomY <= recSecond.BottomY)
+            {
+                resultRectangle.BottomY = recSecond.BottomY;
+            }
+            else
+            {
+                resultRectangle.BottomY = recFirst.BottomY;
+            }
+
+            if (recFirst.TopY <= recSecond.TopY)
+            {
+                resultRectangle.TopY = recFirst.TopY;
+            }
+            else
+            {
+                resultRectangle.TopY = recSecond.TopY;
+            }
+
+            resultRectangle.Height = resultRectangle.TopY - resultRectangle.BottomY;
+
+            return resultRectangle;
+        }
+    }
+}
---
+++ AlgorithmDotnetTest/RectangleIntersection.cs
@@ -0,0 +1,32 @@
+﻿using AlgorithmDotnet.RectangleIntersection;
+using Newtonsoft.Json;
+using NUnit.Framework;
+
+namespace AlgorithmDotnetTest
+{
+    class RectangleIntersection
+    {
+        private AlgorithmRectangleIntersection _algorithmRectangleIntersection;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _algorithmRectangleIntersection = new AlgorithmRectangleIntersection();
+        }
+
+        [TestCase]
+        public void RectangleIntersection_Normal_ReturnExpectedRectangle()
+        {
+            var firstRectangle = new Rectangle(1, 1, 6, 3);
+            var secondRectangle = new Rectangle(5, 2, 3, 6);
+            var expectedRectangle = new Rectangle(5, 2, 2, 2);
+
+            var result = _algorithmRectangleIntersection.RectangleIntersection(firstRectangle, secondRectangle);
+
+            var resultJson = JsonConvert.SerializeObject(result);
+            var expectedRectangleJson = JsonConvert.SerializeObject(expectedRectangle);
+
+            Assert.AreEqual(expectedRectangleJson, resultJson);
+        }
+    }
+}
